e4a93e4fcb24d855b606ea32152e79e774c6b23d
HannahO94 <56817756+HannahO94@users.noreply.github.com>
2020-02-12 14:29:31 +0100

    använder maddes filer eftersom det är den vi lämnar in och skriver rapport om

--- main.js
+++ main.js
@@ -1,165 +1,139 @@
 $(document).ready(function() {
-    //inbygda metoden getjson används för att hämta data från products.json
     $.getJSON("products.json", function(item) {
         let data = item.products;
-        //en loop som hämtar json data och för varje produkt skapas html taggar och produkterna appendas till produkt containern. En köp knapp skapas.
-        //för varje objekt i arrayen ska följande hända
         $.each(data, function(i) {
             $("#product-container").append(
-                '<div class="product-card"' +
-                    "<div>" +
-                    "<img class='card-img-top image'src=" +
+                "<div class='product-card'>" +
+                    "<img class='card-img-top'src=" +
                     data[i].img +
                     ">" +
-                    "<p class='product'>" +
+                    '<span class="product">' +
                     data[i].name +
-                    "  " +
-                    "</p>" +
-                    "<span class='price'>" +
+                    "</span>" +
+                    "<br>" +
+                    '<span class="price">' +
                     data[i].price +
                     "</span>" +
-                    "<span> kr </span>" +
+                    "<span>kr</span>" +
                     "<br>" +
                     '<input type="number" max="10" min="1" value="1" class="product-qty" />' +
-                    '<button class="buy-btn btn-warning" style="width: 50px">köp</button>' +
-                    "</p>" +
-                    "</div>" +
+                    " st" +
+                    "<br>" +
+                    "<br>" +
+                    '<button class="btn btn-warning">Köp</button>' +
                     "</div>"
             );
         });
-        //klick event på köp knappen. Hittar pris och produkt och bild elementen.
-        $(".buy-btn").click(function(event) {
+
+        $(".btn").click(addToCart);
+
+        function addToCart(event) {
             event.preventDefault();
-            // $productParent hittar knappens parent, alltså containern som innehåller all produktinformation
             let $productParent = $(this).parent();
             let $itemName = $productParent.find(".product");
             let $itemPrice = $productParent.find(".price");
-            //sparar ner varje bilds url
-            let $itemImage = $productParent.find("img").attr("src");
-
-            //hittar input för hur många av produkten som ska köpas.
             let $itemQty = $productParent.find("input").val();
+            let $itemImg = $productParent.find(".card-img-top").attr("src");
 
-            // skapar list-element till varje ny produkt i varukorgen. lägger till pris och en ta bort knapp. Varan läggs till sist i listan .
-            // antal produkter som man valt vid köpen hämtas ovan och skickas med till det nya inputfältet som skapas i list itemet i variabeln $itemQty
             $("#cart-list").append(
                 "<li class='cart-row'>" +
+                    "<div class='img-container'>" +
+                    $itemImg +
+                    "</div>" +
                     "<span class='product-name'>" +
                     $itemName.text() +
                     "</span>" +
+                    "<br> " +
                     "<span class='product-price'>" +
                     $itemPrice.text() +
                     "</span>" +
                     "<span>kr</span>" +
-                    "<br>" +
+                    " " +
                     '<input type="number" max="10" min="1" value="' +
                     $itemQty +
                     '" class="input-qty" />' +
-                    //lägger in bildens url i istan med css display none.
-                    "<p class='image-container'>" +
-                    $itemImage +
-                    "</p>" +
-                    '<button class="remove-btn">Ta bort</button>' +
+                    " st" +
+                    "<button class='remove-btn'>❌</button>" +
                     "</li>"
             );
 
-            //resetar inputfältet när man klickat på köp
             $(".product-qty").val("1");
-
-            //klick event för ta bort knapp läggs till varje gång ett nytt item skapas
             $(".remove-btn").click(removeCartItem);
-            //funktionen som räknar ut totalsumman körs varje gång en produkt läggs till i varukorgen
             getTotalCost();
-
-            // event för om antalet i input-qty ändras i varukorgen. då körs funktionen get total cost igen
             $(".input-qty").on("input", getTotalCost);
-        });
-        // funktion för att räkna ut totalsumman när en produkt läggs till i varukorgen. loopar över alla befintliga rader och ränkar ut totalsumman
+        }
+
         function getTotalCost() {
             let $totalPrice = 0;
             let $cartRow = $(".cart-row");
-            //loopar över varje rad i varukorgen
             $($cartRow).each(function() {
-                //hittar priset och antalet på this.cartrow för varje loop
                 let $prodPrice = $(this).find(".product-price");
                 let $prodQty = $(this)
                     .find("input")
                     .val();
-                // gör om från strängar till floats med parseFloat
-                let $pPrice = parseFloat($prodPrice.html());
+                let $pPrice = parseFloat($prodPrice.text());
                 let $pQty = parseFloat($prodQty);
-                //räknar ut den nya totalsumman
                 $totalPrice += $pPrice * $pQty;
             });
-            //lägger till den nya totalen på sidan
             document.getElementById("total").innerHTML =
-                "<strong>SUMMA: </strong>" + $totalPrice + "<br>";
+                "<strong>SUMMA: </strong>" + $totalPrice + "kr" + "<br>";
         }
 
-        //funktion för att ta bort item ur varukorgen. tittar på knappens parent element vilket är list elementet och tar bort det
+        $("#order").click(function() {
+            let $cartRow = $(".cart-row");
+            if ($cartRow.length === 0) {
+                alert("Din varukorg är tom!");
+                return false;
+            } else {
+                storeToLocalStorage();
+                $("li").remove();
+                $("#total").html("<strong>SUMMA: </strong>");
+                alert("Tack för din beställning!");
+            }
+        });
+
         function removeCartItem(event) {
             let buttonClicked = event.target;
             buttonClicked.parentElement.remove();
-            //funktionen som räknar ut totalsumman körs varje gång en produkt tas bort ur varukorgen
             getTotalCost();
         }
-        //tar bort alla produkter ur listan. Räknar ut totalsumman, som blir 0 eftersom varukorgen är tom
+
         $("#remove-all").click(function() {
             $("ul").empty();
             getTotalCost();
         });
-    });
-    //När man klickar på beställ så tas samtliga produkter bort ur varukorgen och man får en alert som tackar för köpet
-    $("#order").click(function() {
-        let $cartRow = $(".cart-row");
-        if ($cartRow.length == 0) {
-            //validerar så det finns produkter i varukorgen innan man kan gå vidare till beställningsbekräftelsen
-            alert("Din varukorg är tom");
-            return false;
-        } else {
-            storeToLocalStorage();
-            $("li").remove();
-            $("#total").html("<strong>SUMMA: 0 </strong>");
-            alert("Tack för din beställning!");
+
+        function storeToLocalStorage() {
+            let arrayStorage = [];
+            let $totalCost = $("#total").text();
+            let $cartRow = $(".cart-row");
+
+            $($cartRow).each(function() {
+                let $prodName = $(this)
+                    .find(".product-name")
+                    .text();
+                let $prodPrice = $(this)
+                    .find(".product-price")
+                    .text();
+                let $prodQty = $(this)
+                    .find("input")
+                    .val();
+                let $img = $(this)
+                    .find(".img-container")
+                    .text();
+
+                let $prodInfo = {
+                    name: $prodName,
+                    price: $prodPrice,
+                    qty: $prodQty,
+                    img: $img
+                };
+
+                arrayStorage.push($prodInfo);
+            });
+
+            localStorage.setItem("arrayStorage", JSON.stringify(arrayStorage));
+            localStorage.setItem("total", JSON.stringify($totalCost));
         }
     });
-    function storeToLocalStorage() {
-        //funktion för att lagra produktinformation i localstorage så att det ska kunna hämtas upp på bekräftelsesidan.
-        let arrayStorage = [];
-        let $cartRow = $(".cart-row");
-        //sparar ner totalsumman i en variabel
-        let $totalCost = $("#total").text();
-
-        //kollar på alla list-items i varukorgen och hittar namn, pris, bild och antal. sparar ner i variabler som ska kunna läggas in i objektet
-        $($cartRow).each(function() {
-            let $productName = $(this)
-                .find(".product-name")
-                .html();
-            let $productPrice = $(this)
-                .find(".product-price")
-                .html();
-            let $productQty = $(this)
-                .find("input")
-                .val();
-            //hittar den gömda bild url:en i varukorgen.
-            let $productImage = $(this)
-                .find(".image-container")
-                .text();
-
-            //gör objekt av alla list items
-            let products = {
-                name: $productName,
-                price: $productPrice,
-                quantity: $productQty,
-                image: $productImage
-            };
-            //pushar in objektet i arrayen.
-            arrayStorage.push(products);
-        });
-
-        // Sparar arrayen i localStorage, ger den en key så man kan komma åt den, stringify konverterar ett javascript value till en json sträng
-        localStorage.setItem("arrayStorage", JSON.stringify(arrayStorage));
-        //sparar totalsumman i localstorge, samma sak görs med den som med arrayen.
-        localStorage.setItem("totalCost", JSON.stringify($totalCost));
-    }
 });
---
+++ order-page.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+    fetchOrder();
+
+    function fetchOrder() {
+        let $order = JSON.parse(localStorage.getItem("arrayStorage"));
+        let $totalCost = JSON.parse(localStorage.getItem("total"));
+
+        let $receipt = $("#receipt");
+
+        for (let i = 0; i < $order.length; i++) {
+            let $prodName = $order[i].name;
+            let $prodPrice = $order[i].price;
+            let $prodQty = $order[i].qty;
+            let $prodImg = $order[i].img;
+
+            $($receipt).append(
+                "<li class='item-name'>" +
+                    "<div class='order-img'>" +
+                    "<img class='item-img' src=" +
+                    $prodImg +
+                    ">" +
+                    "</div>" +
+                    "<div class='order-text'>" +
+                    "<span class='item-qty'>" +
+                    $prodQty +
+                    " st</span>" +
+                    " " +
+                    "<span>" +
+                    $prodName +
+                    "</span>" +
+                    " " +
+                    "<span class='item-price'>" +
+                    $prodPrice +
+                    "kr" +
+                    "</span>" +
+                    "</div>" +
+                    "</li>"
+            );
+        }
+
+        document.getElementById("receipt-total").innerHTML =
+            "<span><h3>" + $totalCost;
+        +"kr" + "</h3></span>";
+    }
+
+    localStorage.clear();
+});
